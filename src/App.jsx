835a0a8847841722da8d730d72f4dd9943994ad8
dinadehane35-maker <dinadehane35@gmail.com>
2026-05-16 04:13:03 +0100

    Fix ngrok header WMS

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,20 @@ import { useState, useEffect, useRef } from 'react'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import './App.css'
+// Fix ngrok header pour WMS
+L.TileLayer.WMSHeader = L.TileLayer.WMS.extend({
+  createTile: function(coords, done) {
+    const url = this.getTileUrl(coords);
+    const tile = document.createElement('img');
+    tile.setAttribute('crossOrigin', 'anonymous');
+    fetch(url, { headers: { 'ngrok-skip-browser-warning': 'true' } })
+      .then(r => r.blob())
+      .then(blob => { tile.src = URL.createObjectURL(blob); done(null, tile); })
+      .catch(e => done(e, tile));
+    return tile;
+  }
+});
+L.tileLayer.wmsHeader = (url, opts) => new L.TileLayer.WMSHeader(url, opts);
 
 const GEOSERVER = 'https://happily-prone-bodacious.ngrok-free.dev/geoserver/guelb_richatt/wms'
 const FONDS = [
@@ -123,7 +137,7 @@ export default function App() {
 
     // Couches WMS
     COUCHES.forEach(c => {
-      const wms = L.tileLayer.wms(GEOSERVER, {
+      const wms = L.tileLayer.wmsHeader(GEOSERVER, {
         layers: c.layer,
         format: 'image/png',
         transparent: true,
